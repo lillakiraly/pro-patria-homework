8da2cb5a2d0cc0df2cc743b7eb43fdc1bf916b7c
lillakiraly <kdotlilla@gmail.com>
2023-04-09 17:20:47 +0200

    Add GeoJSON data context

--- pro-patria-homework/src/App.jsx
+++ pro-patria-homework/src/App.jsx
@@ -1,19 +1,32 @@
-import {useEffect, useState} from 'react'
+import {useEffect, useState, createContext} from 'react'
 import Map from "./components/Map.jsx";
 import "leaflet/dist/leaflet.css";
 import geoDataStore from "../data/geojson/geodatastore.json";
 
 
 function App() {
+
     useEffect(() => {
         if (!localStorage.getItem("geoData")) {
             const geoDataStoreStr = JSON.stringify(geoDataStore);
             localStorage.setItem("geoData", geoDataStoreStr);
         }
     }, []);
+
+    const [geoJsonDataContext, setGeoJsonDataContext] = useState(
+        JSON.parse(localStorage.getItem("geoData")));
+
+    useEffect(() => {
+        localStorage.setItem("geoData", JSON.stringify(geoJsonDataContext));
+    }, [geoJsonDataContext]);
+
     return (
-        <Map/>
+        <GeoJsonDataContext.Provider value={{geoJsonDataContext, setGeoJsonDataContext}}>
+            <Map/>
+        </GeoJsonDataContext.Provider>
     )
 }
+
+export const GeoJsonDataContext = createContext();
 
 export default App;
