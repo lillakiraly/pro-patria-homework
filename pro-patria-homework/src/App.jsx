9500fd7039259a7d537f60a3b02ee42000c594ec
lillakiraly <kdotlilla@gmail.com>
2023-04-09 17:09:17 +0200

    Add leaflet styling

--- pro-patria-homework/src/App.jsx
+++ pro-patria-homework/src/App.jsx
@@ -1,10 +1,12 @@
-import { useState } from 'react'
+import {useState} from 'react'
 import Map from "./components/Map.jsx";
+import "leaflet/dist/leaflet.css";
+
 
 function App() {
-  return (
-     <Map/>
-  )
+    return (
+        <Map/>
+    )
 }
 
 export default App
