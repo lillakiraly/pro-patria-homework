be55088bd38942049d75d43cc77ef69051ea6a4a
lillakiraly <kdotlilla@gmail.com>
2023-04-09 17:15:40 +0200

    Add geojsondata to localstorage

--- pro-patria-homework/src/App.jsx
+++ pro-patria-homework/src/App.jsx
@@ -1,12 +1,19 @@
-import {useState} from 'react'
+import {useEffect, useState} from 'react'
 import Map from "./components/Map.jsx";
 import "leaflet/dist/leaflet.css";
+import geoDataStore from "../data/geojson/geodatastore.json";
 
 
 function App() {
+    useEffect(() => {
+        if (!localStorage.getItem("geoData")) {
+            const geoDataStoreStr = JSON.stringify(geoDataStore);
+            localStorage.setItem("geoData", geoDataStoreStr);
+        }
+    }, []);
     return (
         <Map/>
     )
 }
 
-export default App
+export default App;
