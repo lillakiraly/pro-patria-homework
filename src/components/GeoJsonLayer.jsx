8ac06d948c78705edf2b3adb05bf14703cc38287
lillakiraly <kdotlilla@gmail.com>
2023-04-11 17:56:07 +0200

    Remove comments

--- src/components/GeoJsonLayer.jsx
+++ src/components/GeoJsonLayer.jsx
@@ -8,7 +8,6 @@ function GeoJsonLayer() {
     const {geoJsonDataContext} = useContext(GeoJsonDataContext);
     const [geoJsonLayerKey, setGeoJsonLayerKey] = useState(0);
 
-    //TODO: consult with someone how to rerender the geojson layer without including this useEffect -> tried many options -> the useEffect from App.jsx doesn't rerender it
     useEffect(() => {
         setGeoJsonLayerKey(geoJsonLayerKey + 1)
     }, [geoJsonDataContext]);
